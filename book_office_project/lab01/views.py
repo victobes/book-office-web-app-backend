8712ad2068350e28321cf82a3542d413c5b5bd01
victobes <57673028+victobes@users.noreply.github.com>
2024-09-09 22:33:57 +0300

    upgrade ui design

--- book_office_project/lab01/views.py
+++ book_office_project/lab01/views.py
@@ -1,119 +1,184 @@
 from django.shortcuts import render
 
+MINIO_HOST = "localhost"
+MINIO_PORT = 9000
+MINIO_DIR = "book-office-services-images"
+EXTENSION = ".jpg"
+
 services = [
     {
-        'id': 1, 
-        'title': 'Корректура текста', 
-        'price': 'от 45 руб. за 1000 символов', 
-        'description': 'Корректура текста (корректорская правка) - это исправление орфографических, грамматических и пунктуационных ошибок в тексте, устранение морфологических ошибок (употребления форм склонения, числа, падежа и т.д.), проверку соблюдения правил переноса.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
+        "id": 1,
+        "title": "Корректура текста",
+        "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/1{EXTENSION}',
+        "price": "от 45 руб. за 1000 символов",
+        "description": "Корректура текста (корректорская правка) - это исправление орфографических, грамматических и пунктуационных ошибок в тексте, устранение морфологических ошибок (употребления форм склонения, числа, падежа и т.д.), проверку соблюдения правил переноса.",
     },
     {
-        'id': 2, 
-        'title': 'Дизайн обложки', 
-        'price': 'от 3500 руб.', 
-        'description': 'Разрабатываем дизайн обложки для книги любого жанра.\nСложность: от простой шрифтовой композиции до сложной иллюстрированной.\nТехнические требования: для книг в разных переплетах.\nВыбор: всегда предоставляем три варианта дизайна на выбор заказчику.\nИспользуем лицензионные шрифты и картинки, учитываем пожелания.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
+        "id": 2,
+        "title": "Дизайн обложки",
+        "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/2{EXTENSION}',
+        "price": "от 3500 руб.",
+        "description": "Разрабатываем дизайн обложки для книги любого жанра.\nСложность: от простой шрифтовой композиции до сложной иллюстрированной.\nТехнические требования: для книг в разных переплетах.\nВыбор: всегда предоставляем три варианта дизайна на выбор заказчику.\nИспользуем лицензионные шрифты и картинки, учитываем пожелания.",
     },
     {
-        'id': 3, 
-        'title': 'Вёрстка книги', 
-        'price': 'от 65 руб. за 1 страницу', 
-        'description': 'Верстаем книги любой сложности и любого формата.\nМакеты: полиграфический и электронный.\nСложность: от художественной литературы до научной с формулами.\nДизайн: от незамысловатого до макета с разработанной дизайн-концепцией.\nСоблюдаем технические требования типографии, СанПины и ГОСТы.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
+        "id": 3,
+        "title": "Вёрстка книги",
+        "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/3{EXTENSION}',
+        "price": "от 65 руб. за 1 страницу",
+        "description": "Верстаем книги любой сложности и любого формата.\nМакеты: полиграфический и электронный.\nСложность: от художественной литературы до научной с формулами.\nДизайн: от незамысловатого до макета с разработанной дизайн-концепцией.\nСоблюдаем технические требования типографии, СанПины и ГОСТы.",
     },
     {
-        'id': 4, 
-        'title': 'Иллюстрирование', 
-        'price': 'от 1200 руб.', 
-        'description': 'Работаем с иллюстративным материалом для книги.\nОтрисовка: создаем иллюстрации с нуля по вашему техническому заданию или составляем ТЗ. В разных стилях и техниках.\nПодбор: подбираем иллюстрации из лицензионных стоков под вашу тематику в едином стиле.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
+        "id": 4,
+        "title": "Иллюстрирование",
+        "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/4{EXTENSION}',
+        "price": "от 1200 руб.",
+        "description": "Работаем с иллюстративным материалом для книги.\nОтрисовка: создаем иллюстрации с нуля по вашему техническому заданию или составляем ТЗ. В разных стилях и техниках.\nПодбор: подбираем иллюстрации из лицензионных стоков под вашу тематику в едином стиле.",
     },
 ]
 
 orders = [
     {
-    'id':1,
-    'format': 'A4',
-    'circulation': 500,
-    'selected_services': [
-    {
-        'id': 2, 
-        'title': 'Дизайн обложки', 
-        'price': 'от 3500 руб.', 
-        'description': 'Разрабатываем дизайн обложки для книги любого жанра. Сложность: от простой шрифтовой композиции до сложной иллюстрированной. Технические требования: для книг в разных переплетах. Выбор: всегда предоставляем три варианта дизайна на выбор заказчику. Используем лицензионные шрифты и картинки, учитываем пожелания.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
+        "id": 1,
+        "format": "A4",
+        "circulation": 500,
+        "selected_services": [
+            {
+                "id": 2,
+                "title": "Дизайн обложки",
+                "price": "от 3500 руб.",
+                "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/2{EXTENSION}',
+                "rate": 'Тариф "Базовый"',
+            },
+            {
+                "id": 3,
+                "title": "Вёрстка книги",
+                "price": "от 65 руб. за 1 страницу",
+                "service_img_src": f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/3{EXTENSION}',
+                "rate": 'Тариф "Базовый"',
+            },
+        ],
     },
     {
-        'id': 3, 
-        'title': 'Вёрстка книги', 
-        'price': 'от 65 руб. за 1 страницу', 
-        'description': 'Верстаем книги любой сложности и любого формата. Макеты: полиграфический и электронный. Сложность: от художественной литературы до научной с формулами. Дизайн: от незамысловатого до макета с разработанной дизайн-концепцией. Соблюдаем технические требования типографии, СанПины и ГОСТы.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png'
-    },
-    ]
-    },
-    {
-    'id':2,
-    'format': 'A5',
-    'circulation': 10000,
-    'selected_services': [
-    {
-        'id': 3, 
-        'title': 'Вёрстка книги', 
-        'price': 'от 65 руб. за 1 страницу', 
-        'description': 'Верстаем книги любой сложности и любого формата.\nМакеты: полиграфический и электронный.\nСложность: от художественной литературы до научной с формулами.\nДизайн: от незамысловатого до макета с разработанной дизайн-концепцией.\nСоблюдаем технические требования типографии, СанПины и ГОСТы.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png',
-        'rate': 'Тариф "Базовый"',
-    },
-    {
-        'id': 4, 
-        'title': 'Иллюстрирование', 
-        'price': 'от 1200 руб.', 
-        'description': 'Работаем с иллюстративным материалом для книги.\nОтрисовка: создаем иллюстрации с нуля по вашему техническому заданию или составляем ТЗ. В разных стилях и техниках.\nПодбор: подбираем иллюстрации из лицензионных стоков под вашу тематику в едином стиле.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png',
-        'rate': 'Тариф "Премиум"',
-    },
-    {
-        'id': 1, 
-        'title': 'Корректура текста', 
-        'price': 'от 45 руб. за 1000 символов', 
-        'description': 'Корректура текста (корректорская правка) - это исправление орфографических, грамматических и пунктуационных ошибок в тексте, устранение морфологических ошибок (употребления форм склонения, числа, падежа и т.д.), проверку соблюдения правил переноса.', 
-        'image': 'http://172.19.0.3:9000/server-soft-logos/logo-docker.png',
-        'rate': 'Тариф "Базовый"',
-    },
-    ]
+        "id": 2,
+        "format": "A5",
+        "circulation": 10000,
+        "selected_services": [
+            {
+                "id": 3,
+                "title": "Вёрстка книги",
+                "price": "от 65 руб. за 1 страницу",
+                "rate": 'Тариф "Базовый"',
+            },
+            {
+                "id": 4,
+                "title": "Иллюстрирование",
+                "price": "от 1200 руб.",
+                "rate": 'Тариф "Премиум"',
+            },
+            {
+                "id": 1,
+                "title": "Корректура текста",
+                "price": "от 45 руб. за 1000 символов",
+                "rate": 'Тариф "Базовый"',
+            },
+        ],
     },
 ]
 
+def get_services_list(search_query: str):
+    result = []
+    for service in services:
+        if service["title"].lower().startswith(search_query.lower()):
+            result.append(service)
+            # result[-1][
+            #     "service_img_src"
+            # ] = f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/{service["id"]}{EXTENSION}'
+    return result
+
+
 def get_services_list_page(request):
-    input_text = ""
-    return render(request, 'index.html',
-                  {'data': {
-                      'services': [i for i in services if i["title"].startswith(input_text)],
-                      'count': len(orders[1]['selected_services'])
-                  },})
+    search_query = request.GET.get("search_query", "")
+    current_order_id = 1
+
+    return render(
+        request,
+        "services.html",
+        {
+            "data": {
+                "services": get_services_list(search_query),
+                "count": len(orders[current_order_id - 1]["selected_services"]),
+                "order_id": current_order_id,
+            },
+        },
+    )
+
 
 def get_service_page(request, id):
-    for i in services:
-        if i['id'] == id:
-            return render(request, 'service.html',
-                          {'data': i})
-    render(request, 'service.html')
-    
-    
-def get_order_page(request, id):
-    for order in orders:
-        if order['id'] == id:
-            return render(request, 'order.html',
-                          {'data': {
-                            'id': id,
-                            'format': order['format'],
-                            'circulation': order['circulation'],
-                            'services': order['selected_services']  
-                          },})
-    # if id != 0:
-    #     return render(request, 'order.html')
+    for service in services:
+        if service["id"] == id:
+            # service["service_img_src"] = (
+            #     f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/{service["id"]}{EXTENSION}'
+            # )
+            return render(request, "service.html", {"data": service})
+    render(request, "service.html")
 
-    # return render(request, 'order.html',
-    #               {'data': orders[1]})
+# def get_service_info_by_id(id):
+#     for service in services:
+#         if service['id'] == id:
+#             return service
+        
+# def add_services_info(services_list):
+#     for service in services_list:
+#         if service['id'] in id_list:
+#             service['service_img_src'] = f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/{service["id"]}{EXTENSION}'
+#             selected_services.append(service)
+#     return selected_services
+
+def get_order_page(request, id):
+    id = 1
+    for order in orders:
+        if order["id"] == id:
+            return render(
+                request,
+                "order.html",
+                {
+                    "data": {
+                        "id": id,
+                        "format": order["format"],
+                        "circulation": order["circulation"],
+                        "services": order["selected_services"],
+                    },
+                },
+            )
+
+
+# def get_order_page(request, id):
+#     id = 1
+#     return render(
+#         request,
+#         "order.html",
+#         {"data": get_order_data(id)},
+#     )
+
+
+# def order_page(request, id):
+#     if id != 0:
+#         return render(request, "order.html")
+
+#     return render(request, "order.html", {"data": get_order_data()})
+
+# def get_order_data(id):
+#     for order in orders:
+#         if order["id"] == id:
+#             current_order = order.copy()
+#     selected_services_count = len(current_order["selected_services"])
+#     for service in current_order['selected_services']:
+#         service[
+#             "service_img_src"
+#         ] = f'http://{MINIO_HOST}:{MINIO_PORT}/{MINIO_DIR}/{service["id"]}{EXTENSION}'
+#         service_id = service['id']
+#         service = services[]
+#     return {
+#         "order": current_order,
+#         "selected_services_count": selected_services_count,
+#     }
